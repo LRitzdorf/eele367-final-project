a6e1193ad440d041f73995ffdfd4e892519e7b4a
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2022-04-20 17:47:24 -0600

    control: add opcode values for ALU instructions

--- control_unit.vhd
+++ control_unit.vhd
@@ -63,6 +63,14 @@ begin
         constant LDB_DIR : std_logic_vector := x"89";
         constant STA_DIR : std_logic_vector := x"96";
         constant STB_DIR : std_logic_vector := x"97";
+        constant ADD_AB  : std_logic_vector := x"42";
+        constant SUB_AB  : std_logic_vector := x"43";
+        constant AND_AB  : std_logic_vector := x"44";
+        constant OR_AB   : std_logic_vector := x"45";
+        constant INCA    : std_logic_vector := x"46";
+        constant INCB    : std_logic_vector := x"47";
+        constant DECA    : std_logic_vector := x"48";
+        constant DECB    : std_logic_vector := x"49";
         constant BRA     : std_logic_vector := x"20";
         -- TODO: Add opcode values above this line
         constant NOP     : std_logic_vector := x"00";
