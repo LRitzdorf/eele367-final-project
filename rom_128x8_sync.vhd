034ad1e14b149b867df432616958ff882f305260
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2022-04-20 17:07:45 -0600

    rom: add opcode constants, use them in memory declaration

--- rom_128x8_sync.vhd
+++ rom_128x8_sync.vhd
@@ -12,25 +12,33 @@ end entity;
 
 architecture rom_128x8_sync_arch of rom_128x8_sync is
 
+    constant LDA_IMM : std_logic_vector := x"86";
+    constant LDA_DIR : std_logic_vector := x"87";
+    constant STA_DIR : std_logic_vector := x"96";
+    constant BRA     : std_logic_vector := x"20";
+    -- TODO: Add opcode values above this line
+    constant NOP     : std_logic_vector := x"00";
+    constant HALT    : std_logic_vector := x"FF";
+
     type ROM_Type is array (0 to 127) of std_logic_vector(data_out'left downto 0);
 
     -- NOTE: Program goes in here!
     constant ROM : ROM_Type := (
-        16#00# => x"87",
+        16#00# => LDA_DIR,
         16#01# => x"F0",
 
-        16#02# => x"96",
+        16#02# => STA_DIR,
         16#03# => x"E0",
-        16#04# => x"96",
+        16#04# => STA_DIR,
         16#05# => x"E1",
-        16#06# => x"96",
+        16#06# => STA_DIR,
         16#07# => x"E2",
-        16#08# => x"96",
+        16#08# => STA_DIR,
         16#09# => x"E3",
 
-        16#0A# => x"20",
+        16#0A# => BRA,
         16#0B# => x"00",
-        others => x"FF"
+        others => HALT
     );
 
 begin
