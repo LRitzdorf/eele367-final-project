a10149179189f98e802d38861e0cced773a33fff
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2022-04-20 20:33:24 -0600

    alu: implement using combinational logic

--- alu.vhd
+++ alu.vhd
@@ -15,16 +15,33 @@ end entity;
 
 architecture alu_arch of alu is
 
-    signal full_result : std_logic_vector(width downto 0);
+    signal intermediate : std_logic_vector(width downto 0);
 
 begin
 
-    ALU_PROC : process(In1, In2, Sel)
-    begin
-        -- TODO: Implement everything, add usage notes to README
-        -- FIXME
-        Result <= std_logic_vector(to_unsigned(0, Result'length));
-        NZVC   <= std_logic_vector(to_unsigned(0, NZVC'length));
-    end process;
+    -- TODO: Add usage notes to README
+    with Sel select
+        intermediate <=
+            -- Addition
+            std_logic_vector(unsigned('0' & In1) + unsigned('0' & In2))          when "000",
+            -- Subtraction
+            std_logic_vector(unsigned('0' & In1) - unsigned('0' & In2))          when "001",
+            -- Logic AND
+            '0' & (In1 and In2)                                                  when "010",
+            -- Logic OR
+            '0' & (In1 or In2)                                                   when "011",
+            -- Increment
+            std_logic_vector(unsigned('0' & In2) + to_unsigned(1, In2'length+1)) when "100",
+            -- Decrement
+            std_logic_vector(unsigned('0' & In2) - to_unsigned(1, In2'length+1)) when "101",
+            -- Other (invalid) operation: return zero
+            std_logic_vector(to_unsigned(0, intermediate'length))                when others;
+
+    -- Result generation from intermediate value
+    Result  <= intermediate(intermediate'high-1 downto 0);
+    NZVC(3) <= Result(Result'high);
+    NZVC(2) <= '1' when to_integer(unsigned(Result)) = 0 else '0';
+    NZVC(1) <= '1' when In1(In1'high) = In2(In2'high) and In1(In1'high) /= Result(Result'high) else '0';
+    NZVC(0) <= intermediate(intermediate'high);
 
 end architecture;
